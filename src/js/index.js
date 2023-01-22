adee343f939cc3a7cb94cc9e22f14f428ac8e485
Miguel <mlmiguelleitaoml@gmail.com>
2023-01-22 12:57:54 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botaoTrailer");
+const botaoFecharModal = document.querySelector(".fecharModal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkVideo = video.src;
+
+function alternarModal() {
+  modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+  alternarModal();
+  video.setAttribute("src", linkVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+  alternarModal();
+  video.setAttribute("src", "");
+});
